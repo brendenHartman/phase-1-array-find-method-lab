6f7a47fc2497fa33824cd491844622fb0a581e4a
Brenden Hartman <brendenhart22@gmail.com>
2023-12-18 17:40:52 -0600

    I did it

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+
+function isW(element){
+    return (element.result === "W");
+}
+function superbowlWin(record) {
+     if(record.find(isW)){
+         const elem = record.find(isW);
+         return elem["year"];
+     }
+     else {
+        return record.find(isW);
+     }
+}
